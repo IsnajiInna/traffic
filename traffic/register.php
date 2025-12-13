96ba42d0eece7f0acb765b78baf72c801b2e08b3
Inna <qb202100511@wmsu.edu.ph>
2025-12-13 13:56:21 +0800

    Fix HTML entity encoding in register.php

--- traffic/register.php
+++ traffic/register.php
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register - Traffic Violation System</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <header>
