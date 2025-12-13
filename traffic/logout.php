24938bfe96872e6cdced3c0fc7ad8eb5a817f5cb
Inna <qb202100511@wmsu.edu.ph>
2025-12-13 13:54:00 +0800

    Implement logout confirmation dialog
    Added confirmation prompt for logout action.

--- traffic/logout.php
+++ traffic/logout.php
@@ -1,5 +1,16 @@
 <?php
-include 'config.php';
+if (!isset($_GET['confirm'])) {
+    echo "<script>
+            if (confirm('Are you sure you want to logout?')) {
+                window.location.href = 'logout.php?confirm=1';
+            } else {
+                window.history.back();
+            }
+          </script>";
+    exit();
+}
+
+session_start();
 session_destroy();
 header("Location: login.php");
 exit();
